7d114508133fa0a081afe41a3ee891054e959748
evabr <evabr@Benoit>
2024-02-02 17:06:47 +0100

    Classe Examen élimination des erreurs (sauf concernant images)

--- src/sir5/Examen.java
+++ src/sir5/Examen.java
@@ -12,18 +12,36 @@ import java.util.Calendar;
  * @author agnesvolant
  */
 public class Examen {
+    public enum TypeExamen{
+        RADIOLOGIE_EPAULE, 
+        RADIOLOGIE_HANCHE, 
+        RADIOLOGIE_BASSIN, 
+        RADIOLOGIE_GENOU, 
+        RADIOLOGIE_PIED,
+        RADIOLOGIE_MAIN,
+        RADIOLOGIE_COLONNE,
+        RADIOLOGIE_TETE,
+        RADIOLOGIE_MEMBRE_SUP, 
+        RADIOLOGIE_MEMBRE_INF, 
+        RADIOLOGIE_THORAX;
+    }
 
+    //Images est en commentaire car il faut soit créer une classe soit utiliser le type image dispo sur Java (ce qui semble être adapté au premier abord).
+    //Il faudra qu'on en discute :)
     private Patient patient;
     private String idExam;
     private Professionnel professionnel;
     private String cr;
-    private List<Image> images;
+    //private List <Image> images;
     private Calendar date;
-    private String type;
+    private TypeExamen type;
 
-    public Examen(String idExam, String PHrespo, Date date, String type) {
+    public Examen(Patient patient, String idExam, Professionnel professionnel, String cr, Calendar date, TypeExamen type) {
+        this.patient = patient;
         this.idExam = idExam;
         this.professionnel = professionnel;
+        this.cr = cr;
+        //this.images = images;
         this.date = date;
         this.type = type;
     }
@@ -32,56 +50,48 @@ public class Examen {
         return patient;
     }
 
-    public String getIdExam() {
-        return idExam;
-    }
-
-    public Professionnel professionnel () {
-        return professionnel;
-    }
-
-    public String getCr() {
-        return cr;
-    }
-
-    public <any> getImages() {
-        return images;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public String getType() {
-        return type;
-    }
-
     public void setPatient(Patient patient) {
         this.patient = patient;
+    }
+
+    public String getIdExam() {
+        return idExam;
     }
 
     public void setIdExam(String idExam) {
         this.idExam = idExam;
     }
 
-    public void setPHrespo(String PHrespo) {
-        this.PHrespo = PHrespo;
+    public Professionnel getProfessionnel() {
+        return professionnel;
     }
 
-    public void setCr(CR cr) {
+    public void setProfessionnel(Professionnel professionnel) {
+        this.professionnel = professionnel;
+    }
+
+    public String getCr() {
+        return cr;
+    }
+
+    public void setCr(String cr) {
         this.cr = cr;
     }
 
-    public void setImages(<any>  images) {
-        this.images = images;
+    public Calendar getDate() {
+        return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(Calendar date) {
         this.date = date;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public TypeExamen getType() {
+        return type;
     }
 
+    public void setType(TypeExamen type) {
+        this.type = type;
+    }
+       
 }

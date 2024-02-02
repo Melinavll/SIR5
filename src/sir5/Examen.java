75caee3958bca5817c95bb8dd507ddb6b0257271
agnesvolant <agnesvolant@eduroam-110051.grenet.fr>
2024-02-02 15:26:22 +0100

    ma nouvelle classe examen

---
+++ src/sir5/Examen.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sir5;
+
+/**
+ *
+ * @author agnesvolant
+ */
+public class Examen {
+
+    private Patient patient;
+    private String idExam;
+    private String PHrespo;
+    private CR cr;
+    private List<Image> images;
+    private Date date;
+    private String type;
+
+    public Examen(String idExam, String PHrespo, Date date, String type) {
+        this.idExam = idExam;
+        this.PHrespo = PHrespo;
+        this.date = date;
+        this.type = type;
+    }
+
+    public String getIdExam() {
+        return idExam;
+    }
+
+    public PHrespo PHrespo() {
+        return PHrespo
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getIdCPS() {
+        return idCPS;
+    }
+
+    public String getIdPatient() {
+        return idPatient;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
